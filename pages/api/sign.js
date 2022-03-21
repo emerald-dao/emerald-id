0dbffd7703e4d1204238ff41c4032aab1e2a7d86
jacob-tucker <jacobtucker818@gmail.com>
2022-03-21 19:53:46 -0400

    test again

--- pages/api/sign.js
+++ pages/api/sign.js
@@ -60,30 +60,30 @@ export default async function handler(req, res) {
   // User is now validated //
 
   const { message } = signable;
-  // const decoded = decode(Buffer.from(message.slice(64), 'hex'));
-  // const cadence = decoded[0][0].toString();
-  // const userTxArg = JSON.parse(decoded[0][1][0].toString()).value;
+  const decoded = decode(Buffer.from(message.slice(64), 'hex'));
+  const cadence = decoded[0][0].toString();
+  const userTxArg = JSON.parse(decoded[0][1][0].toString()).value;
 
-  // if (scriptCode.replace(/\s/g, "") !== cadence.replace(/\s/g, "")) {
-  //   return res.status(500).json({ message: 'Script code not supported' })
-  // } else if (user.addr !== userTxArg) {
-  //   return res.status(500).json({ message: 'Incorrect user argument' })
-  // }
+  if (scriptCode.replace(/\s/g, "") !== cadence.replace(/\s/g, "")) {
+    return res.status(500).json({ message: 'Script code not supported' })
+  } else if (user.addr !== userTxArg) {
+    return res.status(500).json({ message: 'Incorrect user argument' })
+  }
 
-  // // If this is initialization, we need to check the discordID passed in.
-  // if (scriptName === 'initializeEmeraldID') {
-  //   // Gets the discord information based on the code
-  //   const userResult = await fetch('https://discord.com/api/users/@me', {
-  //     headers: {
-  //       authorization: `${oauthData.token_type} ${oauthData.access_token}`,
-  //     },
-  //   });
-  //   const info = await userResult.json();
-  //   const discordIDTxArg = JSON.parse(decoded[0][1][1].toString()).value;
-  //   if (info.id !== discordIDTxArg) {
-  //     return res.status(500).json({ message: 'Incorrect discordID' })
-  //   }
-  // }
+  // If this is initialization, we need to check the discordID passed in.
+  if (scriptName === 'initializeEmeraldID') {
+    // Gets the discord information based on the code
+    const userResult = await fetch('https://discord.com/api/users/@me', {
+      headers: {
+        authorization: `${oauthData.token_type} ${oauthData.access_token}`,
+      },
+    });
+    const info = await userResult.json();
+    const discordIDTxArg = JSON.parse(decoded[0][1][1].toString()).value;
+    if (info.id !== discordIDTxArg) {
+      return res.status(500).json({ message: 'Incorrect discordID' })
+    }
+  }
 
   // when the code match , will sign the transaction
   const signature = sign(message);
