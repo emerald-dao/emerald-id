3a53ac7ba4e660b06d3d575413e293a9f9e7ad29
jacob-tucker <jacobtucker818@gmail.com>
2022-03-21 20:46:42 -0400

    config

--- flow/config.js
+++ flow/config.js
@@ -3,7 +3,7 @@ import { config } from "@onflow/fcl";
 config({
   "app.detail.title": "Emerald ID",
   "app.detail.icon": "https://i.imgur.com/DJD0298.png",
-  "accessNode.api": process.env.NEXT_PUBLIC_ACCESS_NODE_API_TESTNET,
-  "discovery.wallet": process.env.NEXT_PUBLIC_DISCOVERY_WALLET_TESTNET,
-  "0xEmeraldIdentity": process.env.NEXT_PUBLIC_CONTRACT, // The account address where the smart contract lives
+  "accessNode.api": "https://testnet.onflow.org",
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
+  "0xEmeraldIdentity": "0xfe433270356d985c", // The account address where the smart contract lives
 })
