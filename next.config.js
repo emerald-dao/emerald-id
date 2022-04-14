358522001a00b1c43bcab8ed1e996531f91ef051
Jacob Tucker <tsnakejake@aol.com>
2022-04-14 04:03:48 -0500

    add redirects for create and reset urls

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/create',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/reset',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
 }
-
-module.exports = nextConfig
