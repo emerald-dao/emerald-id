5b05a6002f9393122301695e28d199f37c8af345
Jacob Tucker <tsnakejake@aol.com>
2022-05-20 09:48:09 -0500

    add reset to differing account

--- components/Create/Differing.jsx
+++ components/Create/Differing.jsx
@@ -1,6 +1,9 @@
 import styles from "../../styles/Create.module.scss";
+import { useFlow } from "../../context/FlowContext";
 
 function Differing({ current, differing }) {
+  const { resetBloctoEmeraldID } = useFlow();
+
   if (current === 'discord') {
     return (
       <div className={styles.differing}>
@@ -15,7 +18,7 @@ function Differing({ current, differing }) {
         <h2>Oops!</h2>
         <p>Your Blocto account is currently mapped to a different Discord account.</p>
         <p>To fix this issue, please reset your EmeraldID below, and try again after.</p>
-        <button style={{backgroundColor: '#fd5c63'}}>Reset</button>
+        <button style={{backgroundColor: '#fd5c63'}} onClick={resetBloctoEmeraldID}>Reset</button>
       </div>
     )
   }
