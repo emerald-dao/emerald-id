4d775cff0f561ad682e6595feb9a43091ea90f0c
jacob-tucker <jacobtucker818@gmail.com>
2022-09-14 01:17:51 -0400

    show whole discord username

--- components/Nav/User.jsx
+++ components/Nav/User.jsx
@@ -8,13 +8,13 @@ function User() {
 
   if (!discordUsername) {
     return (
-      <a className={styles.authentication} href="https://discord.com/api/oauth2/authorize?client_id=955542718124294236&redirect_uri=https%3A%2F%2Fid.ecdao.org%2Fme&response_type=code&scope=identify">Log in</a>
+      <a className={styles.authentication} href="https://discord.com/api/oauth2/authorize?client_id=955542718124294236&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&response_type=code&scope=identify">Log in</a>
     )
   } else {
     return (
       <Link href="/me">
         <a className={`${styles.authentication} ${styles.logout}`}>
-          <span>{discordUsername.length > 10 ? discordUsername.slice(0, 9) + '...' : discordUsername}</span>
+          <span>{discordUsername}</span>
         </a>
       </Link>
     )
