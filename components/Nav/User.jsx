6010c45c3ea857b8c81e40f0d89fcfacbb143030
jacob-tucker <jacobtucker818@gmail.com>
2022-09-14 12:15:42 -0400

    fix error in link

--- components/Nav/User.jsx
+++ components/Nav/User.jsx
@@ -8,7 +8,7 @@ function User() {
 
   if (!discordUsername) {
     return (
-      <a className={styles.authentication} href="https://discord.com/api/oauth2/authorize?client_id=955542718124294236&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&response_type=code&scope=identify">Log in</a>
+      <a className={styles.authentication} href="https://discord.com/api/oauth2/authorize?client_id=955542718124294236&redirect_uri=https%3A%2F%2Fid.ecdao.org%2Fme&response_type=code&scope=identify">Log in</a>
     )
   } else {
     return (
