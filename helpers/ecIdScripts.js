ae181713c8f360654c0ee1c1771b4fc16171028e
jacob-tucker <jacobtucker818@gmail.com>
2022-12-07 14:41:41 -0500

    fix for dapper

--- helpers/ecIdScripts.js
+++ helpers/ecIdScripts.js
@@ -4,7 +4,22 @@ const t = require("@onflow/types");
 const createEmeraldIDTxCode = "import EmeraldIdentity from 0x39e42c67cc851cfb\n\ntransaction(discordID: String) {\n    prepare(admin: AuthAccount, user: AuthAccount) {\n        let administrator = admin.borrow<&EmeraldIdentity.Administrator>(from: EmeraldIdentity.AdministratorStoragePath)\n                                    ?? panic(\"Could not borrow the administrator\")\n        administrator.createEmeraldID(account: user.address, discordID: discordID)\n    }\n\n    execute {\n        log(\"Created EmeraldID\")\n    }\n}";
 
 const createEmeraldID = (wallet) => {
-    const contractName = wallet === 'Blocto' ? 'EmeraldIdentity' : wallet === 'Lilico' ? 'EmeraldIdentityLilico' : wallet === 'Dapper' ? 'EmeraldIdentityDapper' : null;
+    if (wallet === 'Dapper') {
+        return `import EmeraldIdentityDapper from 0x39e42c67cc851cfb
+
+        transaction(discordID: String) {
+            prepare(admin: AuthAccount, user: AuthAccount) {
+                let administrator = admin.borrow<&EmeraldIdentityDapper.Administrator>(from: EmeraldIdentityDapper.AdministratorStoragePath)
+                                            ?? panic("Could not borrow the administrator")
+                administrator.createEmeraldID(account: user.address, discordID: discordID)
+            }
+    
+            execute {
+                log("Created EmeraldID")
+            }
+        }`
+    }
+    const contractName = wallet === 'Blocto' ? 'EmeraldIdentity' : wallet === 'Lilico' ? 'EmeraldIdentityLilico' : null;
     return createEmeraldIDTxCode.replaceAll("EmeraldIdentity", contractName);
 }
 
