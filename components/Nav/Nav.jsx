20672bffc677db00d2cbdf3f71fda70c123ccc5f
jacob-tucker <jacobtucker818@gmail.com>
2023-09-28 15:40:18 -0400

    new emeraldid logo

--- components/Nav/Nav.jsx
+++ components/Nav/Nav.jsx
@@ -30,7 +30,7 @@ function Nav() {
     <nav className={styles.nav}>
       <Link className="flex white-color" href="/">
         <a>
-          <img src="/img/emerald_logo.png" alt="EmeraldID Logo" />
+          <img src="/img/emerald-id-logo.png" alt="EmeraldID Logo" />
           <h1>EmeraldID</h1>
         </a>
       </Link>
